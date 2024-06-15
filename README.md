fc4beb9f3a24610a395f5b4a54a8f87a50c66fda
archibalduk <archibalduk@gmail.com>
2024-06-15 13:20:04 +0100

    Add example: Simple Table View

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ Data can be accessed using [QModelIndex](https://doc.qt.io/qt-6/qmodelindex.html
 ## Author
 [@archibalduk](https://www.github.com/archibalduk)
 
-# Credits
+## Credits
 EHM DAL uses [QXlsx by j2doll](https://github.com/QtExcel/QXlsx). QXlsx is licensed under the MIT license.
 
 ## License
---
+++ examples/01-01_Simple_Table_View/mainwindow.h
@@ -0,0 +1,45 @@
+#pragma once
+
+/* ***************************************
+ * EHM Data Abstraction Layer
+ * by Archibalduk
+ * 
+ * https://github.com/archibalduk/EHM_DAL
+ * 
+ * Example 01.01 - Simple Table View
+ * 
+ * ***************************************/
+
+#include <QAction>
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QTableView>
+
+/// The purpose of this example is to show how easily a database table can be displayed using a QTableView widget.
+/// This example additionally shows how table data can be exported to a csv/xlsx spreadsheet file.
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    // Menu
+    QMenuBar *menu_bar_;
+    QAction *open_database_action_;
+    QAction *open_saved_game_action_;
+    QAction *spreadsheet_export_all_columns_action_;
+    QAction *spreadsheet_export_selected_columns_action_;
+
+    // Table
+    QTableView *table_view_;
+
+private slots:
+    // Slot functions for connecting to signal functions
+    void exportSpreadsheet();
+    void exportSpreadsheetSelectedColumns();
+    void openDatabase();
+    void openSavedGame();
+};
